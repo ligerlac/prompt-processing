18c48fa22c183eb6cad8f6ea25b027bf0dd8943a
ligerlac <lino.oscar.gerlach@cern.ch>
2023-06-02 18:26:52 -0400

    introduced proper logging

--- prompt-processing/joblauncher.py
+++ prompt-processing/joblauncher.py
@@ -2,74 +2,73 @@ import subprocess
 import random
 import socket
 import multiprocessing as mp
+import argparse
+import logging
 
 
-running_jobs = {}
+all_jobs = {}
 
 
-def simulate_htcondor_job(job):
-    # Simulate job running with a subprocess call
+def launch_job(job: str, log_level: int) -> None:
+    logging.getLogger().setLevel(log_level)
     proc = subprocess.Popen(
         # f'echo {job} && sleep {random.randint(1, 5)}',
-        f'echo {job} && sleep {2}',
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        shell=True)
-
-    stdout, stderr = proc.communicate()
-    # Register.running_jobs.remove(job)
-
+        f'echo {job} && sleep {2}', shell=True,
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = proc.communicate()
     if proc.returncode == 0:
-        print(f'[stdout]\n{stdout.decode()}')
+        logging.info(f'job {job} returned {out.decode()}')
     else:
-        print(f'[stderr]\n{stderr.decode()}')
+        logging.error(err.decode())
 
 
-def start_job(job):
-    # Add the job to the Pool
-    pool.apply_async(simulate_htcondor_job, (job,))
+def enqueue_job(line: str) -> str:
+    result = pool.apply_async(launch_job, (line, logging.root.level))
+    all_jobs[line] = result
+    return 'Job started'
 
 
-def run_server():
-    # Create a socket
+def get_running_jobs() -> str:
+    running_jobs = []
+    for j, r in all_jobs.items():
+        if not r.ready():
+            running_jobs.append(j)
+    return str(running_jobs)
+
+
+def process_command(line) -> str:
+    first_word = line.split(' ')[0]
+    return {'submit': enqueue_job(first_word),
+            'count': get_running_jobs()
+            }.get(first_word, 'invalid command')
+
+
+def serve_socket(s: socket.socket()) -> None:
+    conn, _ = s.accept()
+    request = conn.recv(1024)
+    if not request:
+        return
+    line = request.decode()
+    logging.info(f'Received line: {line}')
+    resp = process_command(line)
+    conn.sendall(resp.encode())
+
+
+def main(args):
+    logging.getLogger().setLevel(args.log_level)
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        # Bind the socket to a specific address
-        s.bind(('localhost', 12345))
-
-        # Listen for incoming connections
+        print(f'running server under {args.hostname}:{args.port}')
+        s.bind((args.hostname, args.port))
         s.listen()
-
         while True:
-            # Accept a connection
-            conn, addr = s.accept()
-
-            with conn:
-                print(f'Connected by {addr}')
-                while True:
-                    # Receive a job request
-                    job = conn.recv(1024)
-                    if not job:
-                        break
-                    print(f'Received job: {job.decode()}')
-                    job = job.decode()
-                    if job.startswith('submit'):
-                        # Start the job asynchronously
-                        result = pool.apply_async(simulate_htcondor_job, (job,))
-                        running_jobs[job] = result
-                        conn.sendall('Job started'.encode())
-
-                    elif job.startswith('count'):
-                        l = []
-                        for j, r in running_jobs.items():
-                            if not r.ready():
-                                l.append(j)
-                        conn.sendall(str(l).encode())
-
-                    else:
-                        conn.sendall('invalid command'.encode())
+            serve_socket(s)
 
 
 if __name__ == '__main__':
-    # Create a global Pool with 4 worker processes
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--hostname', type=str, default='localhost')
+    parser.add_argument('--port', type=int, default=12345)
+    parser.add_argument('-l', '--log-level', choices=['DEBUG', 'INFO', 'WARNING'],
+                        default='WARNING')
     pool = mp.Pool(4)
-    run_server()
+    main(parser.parse_args())
