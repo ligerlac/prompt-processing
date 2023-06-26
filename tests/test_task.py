a1151654f1b59ecafb158f383e93395d847b9480
ligerlac <lino.oscar.gerlach@cern.ch>
2023-06-26 16:56:00 +0200

    tests for Task dataclass

---
+++ tests/test_task.py
@@ -0,0 +1,16 @@
+from prompt_processing.task import Task
+
+t1 = Task(input='data/input/test_file.root',
+          output='data/output/test_file_processed.root',
+          command='python scripts/analyze.py data/input/test_file.root')
+
+
+def test_get_field_names():
+    assert 'id' not in Task.get_field_names()
+
+
+def test_get_dict():
+    d = t1.get_dict_wo_id()
+    for key, value in d.items():
+        assert t1.__dict__[key] == d[key]
+    assert 'id' not in t1.get_dict_wo_id()
